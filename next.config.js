f2cdecb3939a6ca108d2657bdae150e52f25d094
Mehmet VAROL <mehmet.varol@gmail.com>
2021-12-21 17:03:35 +0300

    pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
 
 module.exports = withPlugins([], {});
+
+module.exports = withPWA({
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+    },
+});
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,21 @@
-import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-class CustomDocument extends Document {
-    static async getInitialProps(
-        ctx: DocumentContext,
-    ): Promise<DocumentInitialProps> {
-        const initialProps = await Document.getInitialProps(ctx);
-
-        return initialProps;
+class MyDocument extends Document {
+    render(): JSX.Element {
+        return (
+            <Html>
+                <Head>
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/icon.png"></link>
+                    <meta name="theme-color" content="#fff" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
     }
 }
 
-export default CustomDocument;
+export default MyDocument;
