f4d64a5961dd8685c5da0dcf436d594b4b26d96c
Mehmet VAROL <mehmet.varol@gmail.com>
2021-12-21 16:06:57 +0300

    eslint err fixed

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,3 +1,4 @@
-export const Container = ({ children }) => {
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const Container = ({ children }: { children: React.ReactNode }) => {
     return <div className="min-h-screen flex flex-col">{children}</div>;
 };
